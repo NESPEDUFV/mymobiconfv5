0a020af8ed7a57be9ed16d1bcd68ad899f1033ae
Aryel Penido <aryel.cruz@ufv.br>
2021-10-13 20:32:51 -0300

    configurações capacitor

--- src/app/services/firebase/firebase.service.ts
+++ src/app/services/firebase/firebase.service.ts
@@ -1,7 +1,9 @@
+import { OverlayService } from './../overlay.service';
 import { AlertController } from '@ionic/angular';
 import { BehaviorSubject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Push, PushObject, PushOptions } from '@ionic-native/push/ngx';
 
 
 
@@ -14,7 +16,7 @@ export class FirebaseService {
   novaNotif: boolean = false;
   noticiaBehavior = new BehaviorSubject<boolean>(false);
 
-  constructor(public http: HttpClient,private alertCtrl: AlertController) { }
+  constructor(public http: HttpClient,private alertCtrl: AlertController, private push: Push, private overlays: OverlayService) { }
 
   getIdDispositivo() : string{ return this.idDispositivo}
   getNotifica() : boolean{return this.novaNotif}
@@ -26,5 +28,57 @@ export class FirebaseService {
       return this.noticiaBehavior.asObservable();
   }
 
+  pushSetup(){
+    const options: PushOptions = {
+        android: {
+
+            icon: 'http://www.mymobiconf.caf.ufv.br:8080/static/img/icon.png',
+            senderID: '445932221604',
+
+        },
+
+        windows: {},
+        browser: {
+            pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+        }
+     }
+
+     const pushObject: PushObject = this.push.init(options);
+
+
+
+     pushObject.on('notification').subscribe((data: any) =>{
+        this.novaNoticia();
+        if (data.additionalData.coldstart) {
+            this.novaNotifica();
+            this.novaNoticia();
+        }
+       else{
+            this.novaNotifica();
+            this.novaNoticia();}
+
+        if(data.additionalData.foreground){
+           // this.presentAlert()
+            this.novaNotifica();
+            this.novaNoticia();
+        }
+    });
+
+     pushObject.on('registration').subscribe((registration: any) => {
+         console.log('Device registered', registration)
+         this.idDispositivo = registration.registrationId;
+        });
+
+     pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
+    }
+
+    presentAlert() {
+      this.overlays.createAlert(
+        'Nova notícia!',
+        'Você acabou de receber uma nova notícia!',
+        'Dispensar'
+      )
+
+    }
 
 }
